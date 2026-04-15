c3f96d59ef2f502597b0ae4089e400ef1fae0d2c
RasheedBShaik <rasheedbasha.2004@gmail.com>
2026-04-15 22:31:39 +0530

    add vercel deployed link in backend to avoid cors issue

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 // MUST BE AT THE VERY TOP OF THE FILE
-import dns from 'node:dns';
-dns.setServers(['8.8.8.8', '8.8.4.4']); // Force Google DNS to resolve Atlas SRV records
+import dns from "node:dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]); // Force Google DNS to resolve Atlas SRV records
 
 import express from "express";
 import mongoose from "mongoose";
@@ -10,41 +10,57 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import projectRoutes from "./routes/projectRoutes.js";
-import customerRoutes from "./routes/customerRoutes.js"
+import customerRoutes from "./routes/customerRoutes.js";
 
 dotenv.config();
 const app = express();
 
-app.use(cors({
-  origin: "http://localhost:3000", // During development, this ensures no browser blocks you
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}));
-app.use(express.json());
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://user-management-system-five-alpha.vercel.app",
+];
 
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
-app.use("/api/projects", projectRoutes);  
+app.use("/api/projects", projectRoutes);
 app.use("/api/customers", customerRoutes);
 
 // UI Health Check
 app.get("/", (req, res) => {
-    res.send({
-        status: "Backend Live 🚀",
-        database: mongoose.connection.readyState === 1 ? "Connected ✅" : "Disconnected ❌"
-    });
+  res.send({
+    status: "Backend Live 🚀",
+    database:
+      mongoose.connection.readyState === 1 ? "Connected ✅" : "Disconnected ❌",
+  });
 });
 
 const PORT = 5000;
 app.listen(PORT, "0.0.0.0", () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
 
 // Stable Connection Logic
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch((err) => {
     console.error("❌ MongoDB Connection Failed:", err.message);
